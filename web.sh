3837b3a7dcd46870100855ab1bbd81976d9b1c11
Rob Parrott <robparrott@gmail.com>
2013-05-28 11:48:57 -0400

    add a convenience script to fire up the instance in a web browser at http://hostname/

---
+++ web.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# This script ssh's to the specified instance
+
+source include/functions.sh
+
+if [ -z $1 ]; then
+	echo $( basename $0 ): Must specify instance ID.
+	exit 1
+fi
+
+inst_id=$1
+hname=$( get_instance_hostname ${inst_id} )
+
+url="http://${hname}/"
+
+if [ -z ${hname} ]; then
+	echo No hostname for instance id "${inst_id}".
+	exit
+fi
+
+CMD="echo Platform not yet supported ..."
+
+if [ "Darwin" = "$( uname -a  | awk '{print$1}' )" ]; then
+	CMD="open ${url}"
+fi
+
+exec ${CMD}
+
